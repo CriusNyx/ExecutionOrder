cf4aec4f7393432ef03b0989d5fd61e202b2f7de
CriusNyx <RJReynoldsW@gmail.com>
2019-04-11 15:02:14 -0400

    First Commit

---
+++ Scripts/ExecutionOrderControl/ExecutionTimeAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AttributeUsage(AttributeTargets.Class, Inherited = true)]
+public class ExecutionOrderAttribute : Attribute
+{
+    public readonly int order;
+
+    public ExecutionOrderAttribute(ExecutionOrderValue order)
+    {
+        this.order = (int)order;
+    }
+
+    public ExecutionOrderAttribute(int order)
+    {
+        this.order = order;
+    }
+}
+
+public enum ExecutionOrderValue
+{
+    Earliest = -20,
+    Early = -10,
+    Logic = 0,
+    Physics = 10,
+    Camera = 20,
+}
